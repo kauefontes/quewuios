2d9270c00b2a675dde6bf3d12ada221f31e4b7e6
Kaue Pereira <kauefontes@outlook.com>
2024-11-05 01:00:06 -0400

    feat: added method to print string

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -76,4 +76,14 @@ impl Writer {
     fn new_line(&mut self) {
         // TODO
     }
+    pub fn write_string(&mut self, s: &str) {
+        for byte in s.bytes() {
+            match byte {
+                0x20..=0x7e | b'\n' => self.write_byte(byte),
+                _ => self.write_byte(0xfe),
+            }
+        }
+    }
+}
+
 }
